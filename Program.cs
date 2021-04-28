7d111e4078b893697c80738e6c5724b558c53dac
Vitali Voitau <videofag@mail.ru>
2021-04-28 18:52:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Homework_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2000;
+            int b = 20;
+            int t = a;
+            a = b;
+            b = t;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
